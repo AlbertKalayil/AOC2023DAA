5c7bac4818e64492ac124320f4ed6ff61b52e567
AlbertKalayil <albertkalayil2@gmail.com>
2026-07-15 13:59:31 -0600

    day 7 continued

--- 07/day07.py
+++ 07/day07.py
@@ -11,6 +11,7 @@ def readFile(fileName):
     for i in range(len(lines)):
         lines[i] = lines[i].rstrip()
     return lines
+
 class Hand:
     def __init__(self, cards, bid):
         self.cards = cards
@@ -18,7 +19,81 @@ class Hand:
     
     def handtype(self):
         freq = Counter(self.cards)
-        print(freq.most_common())
+        values = list(freq.values())
+        values.sort(reverse=True)
+        val1 = values[0]
+
+        if val1 != 5 and values[1] == 2:
+            val2 = 1
+        
+        else:
+            val2 = 0
+        
+        return val1 * 2 + val2
+    
+    def cardvalue(self):
+        val_dict = {'2':1, '3':2, '4':3, '5':4, '6':5, '7':6, '8':7, '9':8, 'T':9, 'J':10, 'Q':11, 'K':12, 'A':13}
+        result = 0
+
+        for i in range(5):
+            digit = self.cards[4-i]
+            result += pow(13, i) * val_dict[digit]
+
+        return result
+    
+    def handscore(self):
+        return self.cardvalue() + (self.handtype() * pow(13, 5))
+    
+    def __lt__(self, other):
+        return self.handscore() < other.handscore()
+        
+        
+class Hand_Part2:
+    def __init__(self, cards, bid):
+        self.cards = cards
+        self.bid = bid
+    
+    def handtype(self):
+        freq = Counter(self.cards)
+        jacks = freq['J']
+        values = list(freq.values())
+        values.sort(reverse=True)
+        val1 = values[0]
+        
+        if val1 == 5:
+            return 10
+        
+        val2 = values[1]
+
+        score = val1 * 2 + (1 if val2 == 2 else 0)
+
+        if jacks == 0:
+            return val1 * 2 + (1 if val2 == 2 else 0)
+
+        if val1 != 5 and values[1] == 2:
+            val2 = 1
+        
+        else:
+            val2 = 0
+        
+        return val1 * 2 + val2
+    
+    def cardvalue(self):
+        val_dict = {'J':0, '2':1, '3':2, '4':3, '5':4, '6':5, '7':6, '8':7, '9':8, 'T':9, 'Q':11, 'K':12, 'A':13}
+        result = 0
+
+        for i in range(5):
+            digit = self.cards[4-i]
+            result += pow(13, i) * val_dict[digit]
+
+        return result
+    
+    def handscore(self):
+        return self.cardvalue() + (self.handtype() * pow(13, 5))
+    
+    def __lt__(self, other):
+        return self.handscore() < other.handscore()
+
 
 
 
@@ -30,11 +105,30 @@ def part1(lines):
         bid = int(line.split()[1])
         hands.append(Hand(cards,bid))
 
-    hands[0].handtype()
-    return(f"Result of Part 1.")
+    hands.sort()
+    result = 0
+
+    for i, hand in enumerate(hands):
+        result += (i + 1) * hand.bid
+
+    return(f"The total winnings are {result}")
 
 def part2(lines):
-    # Code the solution to part 2 here, returning the answer as a string
+        # Code the solution to part 1 here, returning the answer as a string
+    hands = []
+    for line in lines:
+        cards = line.split()[0]
+        bid = int(line.split()[1])
+        hands.append(Hand_Part2(cards, bid))
+
+    for hand in hands:
+        hand.handtype()
+    # hands.sort()
+    # result = 0
+
+    # for i, hand in enumerate(hands):
+    #     result += (i + 1) * hand.bid
+
 
     return(f"Result of Part 2.")
 
