9a631c7e21c2a359a0d04946d3261e65576f4b5c
Faheem-Raja <faheem.raja@atco.com>
2026-08-05 13:44:34 -0600

    Day 10

--- 10/Day_10.py
+++ 10/Day_10.py
@@ -17,6 +17,9 @@ class Coordinate:
     def char_at(self, map):
         return(map[self.y][self.x])
 
+    def cross(self,other):
+        return self.x * other.y - self.y * other.x
+
 
 def readFile(fileName):
     # Reads the file at fileName and returns a list of lines stripped of newlines
@@ -28,18 +31,85 @@ def readFile(fileName):
 
 def part1(lines):
     # Code the solution to part 1 here, returning the answer as a string
-    location = Coordinate(1, 1)
+    
     dirs = [Coordinate(0,-1), Coordinate(1, 0), Coordinate(0, 1), Coordinate(-1,0)]
-    print(location.char_at(lines))
+    symbols = {
+        '|' : [0,-1,2,-1],
+        '-' : [-1,1,-1,3],
+        'L' : [-1,-1,1,0],
+        'F' : [1,-1,-1,2],
+        '7' : [3,2,-1,-1],
+        'J' : [-1,0,3,-1],
+        'S' : [0,1,2,3] 
+    }
+
+    
 
     for y, line in enumerate(lines):
         if "S" in line:
             current = Coordinate(line.find("S"), y)
-    print(current)
-    return(f"Result of Part 1.")
+    dir = 0
+    steps = 0
+    look = current+dirs[dir]
+    # print(look)
+    while symbols[look.char_at(lines)][dir] == -1:
+        dir+=1
+        look = current+dirs[dir] 
+
+ 
+    while current.char_at(lines)!='S' or steps == 0:
+
+        current = current+dirs[dir]
+        dir = symbols[current.char_at(lines)][dir]
+        steps+=1 
+
+
+    print(steps)
+    return(f"The mid point is {steps//2} steps away")
 
 def part2(lines):
     # Code the solution to part 2 here, returning the answer as a string
+        
+    dirs = [Coordinate(0,-1), Coordinate(1, 0), Coordinate(0, 1), Coordinate(-1,0)]
+    symbols = {
+        '|' : [0,-1,2,-1],
+        '-' : [-1,1,-1,3],
+        'L' : [-1,-1,1,0],
+        'F' : [1,-1,-1,2],
+        '7' : [3,2,-1,-1],
+        'J' : [-1,0,3,-1],
+        'S' : [0,1,2,3] 
+    }
+
+    
+
+    for y, line in enumerate(lines):
+        if "S" in line:
+            current = Coordinate(line.find("S"), y)
+    dir = 0
+    steps = 0
+    look = current+dirs[dir]
+    # print(look)
+    while symbols[look.char_at(lines)][dir] == -1:
+        dir+=1
+        look = current+dirs[dir] 
+
+    area = 0
+    corners = []
+
+    while current.char_at(lines)!='S' or steps == 0:
+        if current.char_at(lines) in "JFL7S":
+            corners.append(current)
+        current = current+dirs[dir]
+        dir = symbols[current.char_at(lines)][dir]
+        steps+=1 
+
+    for i in range(len(corners)):
+        area += corners[i].cross(corners[(i+1)%len(corners)])
+
+
+    
+    return(f"The inside area is {abs(area//2)-steps//2 +1} ")
 
     return(f"Result of Part 2.")
 
